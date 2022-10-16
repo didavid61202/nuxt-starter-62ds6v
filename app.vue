ff7a094213ca7f9967248c146198d62bab8178f7
didavid61202 <didavid61202@gmail.com>
2022-10-17 00:50:35 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+const userId = ref({ id: 'abc' })
+const { data } = useLazyFetch(`/api/user/${userId.value.id}/other`)
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>

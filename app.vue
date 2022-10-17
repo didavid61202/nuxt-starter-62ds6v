653b7f9bdefe654ebbd5894ed83b166d9ba06b66
Gion Rubitschung INI-EOS-OEN <gion.rubitschung@swisscom.com>
2022-10-17 17:01:41 +0200

    added put request

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-
-const userId = ref({ id: 'userId' })
-const { data } = useLazyFetch('/api/user/:id')
-// const { data } = useLazyFetch(`/api/user/${userId.value.id}`)
+const userId = ref({ id: 'userId' });
+// const { data } = useLazyFetch('/api/user/:id')
+const { data } = useLazyFetch(`/api/user/${userId.value.id}`);
+const { data: msg } = useLazyFetch(`/api/user/${userId.value.id}`, {
+  method: 'PUT',
+});
 // const { data } = useLazyFetch(`/api/user/${userId.value.id}/nestRoute`)
-
 </script>
 
 <template>
